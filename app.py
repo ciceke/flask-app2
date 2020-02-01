716c964341b84d6aa403cbdca07f6fa3890ce920
ciceke <ecicek@gmail.com>
2020-02-01 14:57:33 +0300

    db added

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import os
 from flask import Flask
-app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+import config
 
+app = Flask(__name__)
+
+#app.config.from_object(Config())
+app.config['DATABASE'] = os.path.join(app.instance_path, 'flask-app.sqlite')
+
+try:
+	os.makedirs(app.instance_path)
+except OSError:
+	pass
 
 @app.route('/')
 def hello():
@@ -11,6 +19,9 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+import db
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -1,27 +1,22 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-
+    SECRET_KEY='dev'
+    #DATABASE=os.path.join(app.instance_path, 'flask-app.sqlite')
 
 class ProductionConfig(Config):
     DEBUG = False
-
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
 
 class TestingConfig(Config):
     TESTING = True
